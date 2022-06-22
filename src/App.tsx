1d5db8368663fda6acc60f3cbc77cf5c2b8945a7
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-22 22:03:40 +0900

    update: コメントしたコードを削除

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,5 @@ import Confetti from "react-confetti";
 
 export default function App() {
   const { width, height } = useWindowSize();
-  return (
-    <Confetti width={width} height={height} />
-    // <div className="App">
-    //   <h1>Hello CodeSandbox</h1>
-    //   <h2>Start editing to see some magic happen!</h2>
-    // </div>
-  );
+  return <Confetti width={width} height={height} />;
 }

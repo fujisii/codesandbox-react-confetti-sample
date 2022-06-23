977167e9c509b391636d2aa98d71a8267afdd8d3
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-23 12:55:17 +0900

    実験中

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,27 @@ import "./styles.css";
 import useWindowSize from "react-use/lib/useWindowSize";
 import Confetti from "react-confetti";
 
+// https://github.com/alampros/react-confetti
+
 export default function App() {
   const { width, height } = useWindowSize();
-  return <Confetti width={width} height={height} />;
+  const gravity = 0.1;
+  const numberOfPieces = 200;
+  const confettiSource = {
+    x: width / 3,
+    y: height / 2,
+    w: width / 2,
+    h: height / 2
+  };
+  const recycle = false;
+  return (
+    <Confetti
+      width={width}
+      height={height}
+      numberOfPieces={numberOfPieces}
+      confettiSource={confettiSource}
+      recycle={recycle}
+      gravity={gravity}
+    />
+  );
 }

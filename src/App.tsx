fe63d8cb084c3180e796b89aff7f6375956c4f78
fujisii <51264835+fujisii@users.noreply.github.com>
2022-06-24 12:51:44 +0900

    左下から紙吹雪が舞うようにした

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,24 @@ import Confetti from "react-confetti";
 // https://github.com/alampros/react-confetti
 
 export default function App() {
+  // 要素の幅, 高さ
   const { width, height } = useWindowSize();
+  // 重力
   const gravity = 0.1;
+  // 一度に放出される紙吹雪の数
   const numberOfPieces = 200;
+  // 紙吹雪がスポーンする長方形。デフォルトは上部にあります。
   const confettiSource = {
-    x: width / 3,
-    y: height / 2,
-    w: width / 2,
-    h: height / 2
+    x: 0,
+    y: height,
+    w: 0,
+    h: height
   };
-  const recycle = false;
+  const recycle = true;
+  // 紙吹雪が水平方向に放出される値の範囲。正の数は右向き、負の数は左向きです。
+  const initialVelocityX = { min: 5, max: 5 };
+  // 紙吹雪が垂直に放出される値の範囲。正の数は下向き、負の数は上向きです。
+  const initialVelocityY = { min: 20, max: -20 };
   return (
     <Confetti
       width={width}
@@ -24,6 +32,8 @@ export default function App() {
       confettiSource={confettiSource}
       recycle={recycle}
       gravity={gravity}
+      initialVelocityX={initialVelocityX}
+      initialVelocityY={initialVelocityY}
     />
   );
 }
